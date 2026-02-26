3a457ba640906fbf1705eae9a933eb3656d42fec
ArturErnandes <al211205@yandex.ru>
2026-02-27 01:05:22 +0300

    Добавлены классы для переклички

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-
+from aiogram.fsm.state import State, StatesGroup
 
 @dataclass(frozen=True)
 class DbConfig:
@@ -14,3 +14,18 @@ class DbConfig:
 class User:
     id: int
     name: str
+
+
+class RollCallStates(StatesGroup):
+    waiting_subject = State()
+
+
+@dataclass
+class RollCall:
+    roll_id: str
+    subject: str
+    users: dict[int, str]
+    present: set[int]
+    absent: set[int]
+    report_sent: bool
+    admin_message_id: int | None

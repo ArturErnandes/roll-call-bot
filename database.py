81d90c0571cf2f038d14c12451f6356722541698
ArturErnandes <al211205@yandex.ru>
2026-02-26 20:31:27 +0300

    Добавлена функция получения списка группы

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy import text
 
 from config import db_data
+from classes import User
 from logger import get_logger
 
 logger = get_logger(__name__)
@@ -10,3 +11,24 @@ engine = create_async_engine(
     f'postgresql+asyncpg://{db_data.admin}:{db_data.password}@{db_data.host}:{db_data.port}/{db_data.db_name}')
 
 new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_users_list_db():
+    query = text("SELECT * FROM users")
+
+    async with new_session() as session:
+        try:
+            result = await session.execute(query)
+            users_list: list[User] = []
+
+            for row in result:
+                users_list.append(User(
+                    id = row.user_id,
+                    name = row.name,
+                ))
+            logger.info("Список группы успешно получен")
+            return users_list
+
+        except Exception as e:
+            logger.error(f"Ошибка при получении списка группы: {str(e)}")
+            return []
